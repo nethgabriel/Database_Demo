7a743e730b41a59c0a8ad8939582cdc4ecaee804
Kenneth Tapang <kennethgabtapang@gmail.com>
2023-06-06 19:40:22 +0800

    Add project files.

---
+++ Database_Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EnrollmentDataBaseEntities())
+            {
+
+                var query = from b in db.StudentProfiles
+                            orderby b.FirstName
+                            select b;
+
+                Console.WriteLine("All All student in the database:");
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.FirstName + " " + item.LastName);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
